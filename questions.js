c5bac2cbd609e2d58fc50f1403943f535f0de681
Antonioemannuel <antonioemannuel13@gmail.com>
2023-05-02 01:04:53 -0300

    test

---
+++ questions.js
@@ -0,0 +1,164 @@
+export default [
+  {
+    question: "O que é um sistema operacional?",
+    answers: [
+      { option: "Um software para edição de textos", correct: false },
+      { option: "Um software para gerenciamento de bancos de dados ", correct: false },
+      { option: "Um software que controla o hardware e os recursos de um computador ", correct: true },
+      { option: "Um software para reprodução de mídia", correct: false },
+    ],
+  },
+  {
+    question: "Qual é a função do kernel de um sistema operacional?",
+    answers: [
+      { option: "Gerenciar os dispositivos de entrada e saída", correct: false },
+      { option: "Controlar o acesso à rede", correct: false },
+      { option: " Realizar operações de segurança ", correct: false },
+      { option: " Gerenciar os recursos do hardware ", correct: true },
+    ],
+  },
+  {
+    question: "Qual é o nome do sistema operacional mais utilizado no mundo em computadores pessoais?",
+    answers: [
+      { option: "MacOS", correct: false },
+      { option: "Linux", correct: false },
+      { option: "Windows", correct: true },
+      { option: "iOS", correct: false },
+    ],
+  },
+  {
+    question: "Qual é a função do gerenciador de processos de um sistema operacional?",
+    answers: [
+      { option: "Gerenciar as atualizações de software", correct: false },
+      { option: "Gerenciar os recursos do hardware", correct: false },
+      { option: "Gerenciar os arquivos do sistema", correct: false },
+      { option: "Gerenciar os programas em execução", correct: true },
+    ],
+  },
+  {
+    question: "O que é um arquivo de sistema em um sistema operacional?",
+    answers: [
+      { option: "Um arquivo que contém dados do usuário", correct: false },
+      { option: "Um arquivo que contém configurações do sistema", correct: true },
+      { option: "Um arquivo que contém programas de terceiros", correct: false },
+      { option: "Um arquivo que contém backups do sistema", correct: false },
+    ],
+  },
+  {
+    question: "O que é a memória RAM em um computador?",
+    answers: [
+      { option: "Um dispositivo de armazenamento permanente", correct: false },
+      { option: "Um dispositivo de entrada", correct: false },
+      { option: "Um dispositivo de saída", correct: false },
+      { option: "Um tipo de memória volátil", correct: true },
+    ],
+  },
+  {
+    question: "O que é um sistema de arquivos em um sistema operacional?",
+    answers: [
+      { option: "Um sistema para gerenciar arquivos em rede ", correct: false },
+      { option: "Um sistema para comprimir arquivos", correct: false },
+      { option: "Um sistema para organizar e armazenar arquivos em um dispositivo de armazenamento ", correct: true },
+      { option: "Um sistema para converter arquivos entre diferentes formatos", correct: false },
+    ],
+  },
+  {
+    question: "O que é um driver em um sistema operacional?",
+    answers: [
+      { option: "Um programa para gerenciar as atualizações do sistema", correct: false },
+      { option: "Um programa para gerenciar a segurança do sistema", correct: false },
+      { option: "Um programa para gerenciar os dispositivos de hardware", correct: true },
+      { option: "Um programa para gerenciar as configurações do sistema", correct: false },
+    ],
+  },
+  {
+    question: "Qual sistema operacional tem código-fonte aberto?",
+    answers: [
+      { option: "Windows", correct: false },
+      { option: "Linux", correct: true },
+      { option: "Ambos", correct: false },
+      { option: "Nenhum", correct: false },
+    ],
+  },
+  {
+    question: "Qual sistema operacional é mais utilizado em desktops e laptops?",
+    answers: [
+      { option: "Windows", correct: true },
+      { option: "Linux", correct: false },
+      { option: "Ambos", correct: false },
+      { option: "Nenhum", correct: false },
+    ],
+  },
+  {
+    question: "Qual sistema operacional é mais utilizado em servidores?",
+    answers: [
+      { option: "Windows", correct: false },
+      { option: "Linux", correct: true },
+      { option: "Ambos", correct: false },
+      { option: "Nenhum", correct: false },
+    ],
+  },
+  {
+    question: "Qual sistema operacional é conhecido por ser mais personalizável?",
+    answers: [
+      { option: "Windows", correct: false },
+      { option: "Linux", correct: true },
+      { option: "Ambos", correct: false },
+      { option: "Nenhum", correct: false },
+    ],
+  },
+  {
+    question: "Qual sistema operacional é conhecido por ser mais estável e seguro?",
+    answers: [
+      { option: "Windows", correct: false },
+      { option: "Linux", correct: true },
+      { option: "Ambos", correct: false },
+      { option: "Nenhum", correct: false },
+    ],
+  },
+  {
+    question: "Qual sistema operacional possui uma interface gráfica de usuário mais intuitiva?",
+    answers: [
+      { option: "Windows", correct: true },
+      { option: "Linux", correct: false },
+      { option: "Ambos", correct: false },
+      { option: "Nenhum", correct: false },
+    ],
+  },
+  {
+    question: "Qual sistema operacional permite que o usuário personalize mais facilmente as configurações de rede?",
+    answers: [
+      { option: "Windows", correct: false },
+      { option: "Linux", correct: true },
+      { option: "Ambos", correct: false },
+      { option: "Nenhum", correct: false },
+    ],
+  },
+  {
+    question: "Qual sistema operacional tem mais opções de jogos disponíveis?",
+    answers: [
+      { option: "Windows", correct: true },
+      { option: "Linux", correct: false },
+      { option: "Ambos", correct: false },
+      { option: "Nenhum", correct: false },
+    ],
+  },
+  {
+    question: "Qual sistema operacional é conhecido por ser mais leve e consumir menos recursos do computador?",
+    answers: [
+      { option: "Windows", correct: false },
+      { option: "Linux", correct: true },
+      { option: "Ambos", correct: false },
+      { option: "Nenhum", correct: false },
+    ],
+  },
+  {
+    question: "Qual sistema operacional é mais utilizado em supercomputadores?",
+    answers: [
+      { option: "Windows", correct: false },
+      { option: "Linux", correct: true },
+      { option: "Ambos", correct: false },
+      { option: "Nenhum", correct: false },
+    ],
+  },
+];
